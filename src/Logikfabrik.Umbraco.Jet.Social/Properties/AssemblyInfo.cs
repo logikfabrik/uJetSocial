2d7ab192630d819a4eb6eeff79186b74c96c021c
logikfabrik <anton@logikfabrik.se>
2015-10-04 18:40:41 +0200

    Initial setup.

--- src/Logikfabrik.Umbraco.Jet.Social/Properties/AssemblyInfo.cs
+++ src/Logikfabrik.Umbraco.Jet.Social/Properties/AssemblyInfo.cs
@@ -1,36 +1,21 @@
-﻿using System.Reflection;
+﻿// <copyright file="AssemblyInfo.cs" company="Logikfabrik">
+//   Copyright (c) 2015 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Logikfabrik.Umbraco.Jet.Social")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Logikfabrik.Umbraco.Jet.Social")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Anton Frennevi")]
+[assembly: AssemblyProduct("Umbraco Jet Social")]
+[assembly: AssemblyCopyright("2015 Anton Frennevi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7f0c9f02-d55d-4969-87bd-00528349e05d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: InternalsVisibleTo("Logikfabrik.Umbraco.Jet.Social.Test")]
