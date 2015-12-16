10ec560e5eee38056b63142b82a93abaa6c479ef
logikfabrik <anton@logikfabrik.se>
2015-12-16 07:21:05 +0100

    Added demo project.

---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/Application.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Application.cs" company="Logikfabrik">
+//   Copyright (c) 2015 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Umbraco.Jet.Social.Web
+{
+    using umbraco.businesslogic;
+    using umbraco.interfaces;
+
+    /// <summary>
+    /// The <see cref="Application" /> class.
+    /// </summary>
+    [Application("uJetSocial", "uJetSocial", null)]
+    public class Application : IApplication
+    {
+    }
+}
