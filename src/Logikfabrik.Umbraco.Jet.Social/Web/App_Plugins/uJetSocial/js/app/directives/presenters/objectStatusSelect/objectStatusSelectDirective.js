0aa08bd074cc18aaf19bb613d38ce82d8d595544
logikfabrik <anton@logikfabrik.se>
2016-02-07 13:31:48 +0100

    Directive rewrite.

--- src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/presenters/objectStatusSelect/objectStatusSelectDirective.js
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/presenters/objectStatusSelect/objectStatusSelectDirective.js
@@ -1,16 +1,26 @@
-﻿angular.module("umbraco.directives")
-    .directive("ujetObjectStatusSelect", [
-        "_",
-        function (_) {
-            return {
-                restrict: "E",
-                templateUrl: "/App_Plugins/uJetSocial/js/app/directives/presenters/objectStatusSelect/objectStatusSelectView.html",
-                scope: {
-                    ngModel: "="
-                },
-                link: function (scope, element, attrs) {
-                    scope._ = _;
-                }
-            };
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("umbraco.directives")
+        .directive("ujetObjectStatusSelect", ujetObjectStatusSelect);
+
+    ujetObjectStatusSelect.$inject = ["_"];
+
+    function ujetObjectStatusSelect(_) {
+        var directive = {
+            restrict: "E",
+            templateUrl: "/App_Plugins/uJetSocial/js/app/directives/presenters/objectStatusSelect/objectStatusSelectView.html",
+            scope: {
+                ngModel: "="
+            },
+            link: link
+        };
+
+        return directive;
+
+        function link(scope, element, attrs) {
+            scope._ = _;
         }
-    ]);
+    };
+})();
--- src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/presenters/value/valueDirective.js
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/presenters/value/valueDirective.js
@@ -1,16 +1,26 @@
-﻿angular.module("umbraco.directives")
-    .directive("ujetValue", [
-        "moment",
-        function (moment) {
-            return {
-                restrict: "E",
-                templateUrl: "/App_Plugins/uJetSocial/js/app/directives/presenters/value/valueView.html",
-                scope: {
-                    value: "@"
-                },
-                link: function (scope, element, attrs) {
-                    scope.isDate = moment(scope.value, moment.ISO_8601, true).isValid();
-                }
-            };
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("umbraco.directives")
+        .directive("ujetValue", ujetValue);
+
+    ujetValue.$inject = ["moment"];
+
+    function ujetValue(moment) {
+        var directive = {
+            restrict: "E",
+            templateUrl: "/App_Plugins/uJetSocial/js/app/directives/presenters/value/valueView.html",
+            scope: {
+                value: "@"
+            },
+            link: link
+        };
+
+        return directive;
+
+        function link(scope, element, attrs) {
+            scope.isDate = moment(scope.value, moment.ISO_8601, true).isValid();
         }
-    ]);
+    };
+})();
