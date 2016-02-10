744eb781b0fb8b9c46e7ee945b624f5a72a2ae90
logikfabrik <anton@logikfabrik.se>
2016-02-10 09:48:05 +0100

    Added comment picker.

---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/pickers/commentPicker/commentPickerDir.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("umbraco.directives")
+        .directive("ujetCommentPicker", ujetCommentPickerDir);
+
+    function ujetCommentPickerDir() {
+        var directive = {
+            restrict: "E",
+            templateUrl: "/App_Plugins/uJetSocial/js/app/directives/pickers/commentPicker/commentPickerView.html",
+            scope: true,
+            controller: "ujetCommentPickerDirCtrl"
+        };
+
+        return directive;
+    };
+})();
---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/pickers/commentPicker/commentPickerDirCtrl.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("umbraco")
+        .controller("ujetCommentPickerDirCtrl", ujetCommentPickerDirCtrl);
+
+    ujetCommentPickerDirCtrl.$inject = ["$scope", "$controller", "ujetCommentFactory", "queryService"];
+
+    function ujetCommentPickerDirCtrl($scope, $controller, objectFactory, queryService) {
+        $controller('ujetPickerCtrl', {
+            $scope: $scope,
+            objectFactory: objectFactory,
+            queryService: queryService
+        });
+    };
+})();
