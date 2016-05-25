ee4a059363a421b495adefb25c4da0b542a783a9
logikfabrik <anton@logikfabrik.se>
2016-05-25 07:30:30 +0200

    Added component files for members.

---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/components/individualMember/individualMemberCreateCtrl.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco")
+        .controller("ujetIndividualMemberCreateCtrl", ujetIndividualMemberCreateCtrl);
+
+    ujetIndividualMemberCreateCtrl.$inject = ["$scope", "$controller", "$location", "notificationsService", "ujetIndividualMemberFactory"];
+
+    function ujetIndividualMemberCreateCtrl($scope, $controller, $location, notificationsService, ujetIndividualMemberFactory) {
+        $controller("ujetObjectCreateCtrl", {
+            $scope: $scope,
+            $location: $location,
+            notificationsService: notificationsService,
+            config: {
+                objectFactory: ujetIndividualMemberFactory,
+                path: "/uJetSocial/individualMember/dashboard/-1",
+                createSuccessMessage: "Member created",
+                createErrorMessage: "Member could not be created"
+            }
+        });
+    };
+})();
---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/components/individualMember/individualMemberDashboardCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco")
+        .controller("ujetIndividualMemberDashboardCtrl", ujetIndividualMemberDashboardCtrl);
+
+    ujetIndividualMemberDashboardCtrl.$inject = ["$routeParams", "navigationService"];
+
+    function ujetIndividualMemberDashboardCtrl($routeParams, navigationService) {
+        navigationService.syncTree({ tree: "individualMember", path: ["-1", $routeParams.id], forceReload: false });
+    };
+})();
