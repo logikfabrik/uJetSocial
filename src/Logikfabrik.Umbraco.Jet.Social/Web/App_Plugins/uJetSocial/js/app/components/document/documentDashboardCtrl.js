91f2ad907b2eb52441ed48ffb956453266466dfe
logikfabrik <anton@logikfabrik.se>
2016-06-01 07:38:45 +0200

    Added dashboards for documents and media.

---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/components/document/documentDashboardCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco")
+        .controller("ujetDocumentDashboardCtrl", ujetDocumentDashboardCtrl);
+
+    ujetDocumentDashboardCtrl.$inject = ["$routeParams", "navigationService"];
+
+    function ujetDocumentDashboardCtrl($routeParams, navigationService) {
+        navigationService.syncTree({ tree: "ujetDocument", path: ["-1", $routeParams.id], forceReload: false });
+    };
+})();
---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/components/media/mediaDashboardCtrl.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco")
+        .controller("ujetMediaDashboardCtrl", ujetMediaDashboardCtrl);
+
+    ujetMediaDashboardCtrl.$inject = ["$routeParams", "navigationService"];
+
+    function ujetMediaDashboardCtrl($routeParams, navigationService) {
+        navigationService.syncTree({ tree: "ujetMedia", path: ["-1", $routeParams.id], forceReload: false });
+    };
+})();
