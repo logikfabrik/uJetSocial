62bd4fc75a7a48473518b4e63b44268f0af52b32
logikfabrik <anton@logikfabrik.se>
2016-08-11 18:22:42 +0200

    #13 Began working on the spinner.

---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/spinner/spinnerDir.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco.directives")
+        .directive("ujetSpinner", ujetSpinnerDir);
+
+    function ujetSpinnerDir() {
+        var directive = {
+            restrict: "E",
+            templateUrl: "/App_Plugins/uJetSocial/js/app/directives/spinner/spinnerView.html",
+            scope: true,
+            controller: "ujetSpinnerDirCtrl"
+        };
+
+        return directive;
+    };
+})();
---
+++ src/Logikfabrik.Umbraco.Jet.Social/Web/App_Plugins/uJetSocial/js/app/directives/spinner/spinnerDirCtrl.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    // ReSharper disable once UndeclaredGlobalVariableUsing
+    angular
+        .module("umbraco")
+        .controller("ujetSpinnerDirCtrl", ujetSpinnerDirCtrl);
+
+    ujetSpinnerDirCtrl.$inject = ["$scope", "$http"];
+
+    function ujetSpinnerDirCtrl($scope, $http) {
+        var vm = {
+            isLoading: isLoading
+        };
+
+        $scope.vm = vm;
+
+        function isLoading() {
+            return $http.pendingRequests.length > 0;
+        }
+    };
+})();
